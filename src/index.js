abff8631f9b4cfbb660716099ffe135211b6a14f
jiri <kiviniemi.jiri@gmail.com>
2020-01-10 15:15:59 +0200

    minor UI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import 'babel-polyfill';
 import { AsyncWorker } from './lib/asyncWorker';
 
 const worker = new AsyncWorker('worker.js');
+const drawer = document.querySelector('#drawer');
 
 document.querySelector('#ping').addEventListener('click', e => {
   worker
-    .post({ action: 'pinfoog', payload: '[PING]' })
+    .post({ action: 'ping', payload: '[PING]' })
     .then(result => console.log(result, 'resolved'))
     .catch(e => console.log(e));
 });
@@ -18,3 +19,7 @@ document.querySelector('#pong').addEventListener('click', e => {
     .then(result => console.log(result, 'resolved'))
     .catch(e => console.log(e));
 });
+
+document
+  .querySelector('#menu-toggle')
+  .addEventListener('click', () => drawer.classList.toggle('active'));
