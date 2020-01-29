6e581713a847e0936172e278998ffd2f23c822f1
jiri <kiviniemi.jiri@gmail.com>
2020-01-29 08:24:34 +0200

    UI updates

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import './style.scss';
 import 'babel-polyfill';
 import './routing-module';
 import './worker-module';
+
+document.querySelectorAll('.toggle-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+    const target = button.getAttribute('toggle-target');
+    const element = document.querySelector(target);
+    if (element) {
+      element.classList.toggle('active');
+    }
+  });
+});
+
+document.querySelectorAll('#drawer__nav router-link').forEach(link => {
+  link.addEventListener('click', () => {
+    link.closest('#drawer').classList.remove('active');
+    document.querySelector('.toggle-btn[toggle-target="#drawer"]').classList.remove('active');
+  });
+});
